5bcdafd9dfad43c3628238d430e611f0c57b2807
Cabbie1337 <98100268+Cabbie1337@users.noreply.github.com>
2022-03-09 12:31:16 +0100

    meaw meaw

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        '38': 'repeat(38,minmax(0,1fr))'
+      }
+    },
+  },
+  plugins: [],
+}
